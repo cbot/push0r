e65937e454641de051f8e4f0c31df184b096880b
Kai Straßmann <derkai@gmail.com>
2016-06-24 14:20:00 +0200

    removed old push0r file

--- lib/push0r.rb
+++
@@ -1,8 +0,0 @@
-require 'socket'
-require 'openssl'
-require 'json'
-
-require_relative 'push0r/FlushResult'
-require_relative 'push0r/Queue'
-require_relative 'push0r/Service'
-require_relative 'push0r/PushMessage'
